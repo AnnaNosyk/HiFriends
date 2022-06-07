5e7553fb24c2bdc0100586ea2b7ffd8fecb82a43
Anna Nosyk <anna.nosyk89@gmail.com>
2022-06-07 17:08:21 +0200

    Initial Commit

---
+++ HiFriends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HiFriends
+//
+//  Created by Anna Nosyk on 07/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
