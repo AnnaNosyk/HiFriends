dc174806ef333dafdf877c42ac384ac4b833863d
Anna Nosyk <anna.nosyk89@gmail.com>
2022-06-13 17:31:34 +0200

    -  tabBar Controller for chats

---
+++ HiFriends/Controllers/ListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewController.swift
+//  HiFriends
+//
+//  Created by Anna Nosyk on 13/06/2022.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+}
---
+++ HiFriends/Controllers/PeoleViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PeoleViewController.swift
+//  HiFriends
+//
+//  Created by Anna Nosyk on 13/06/2022.
+//
+
+import UIKit
+
+class PeoleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    
+    }
+  
+   
+
+}
